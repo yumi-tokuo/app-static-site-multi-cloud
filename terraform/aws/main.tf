853ad2cfdb71375cd2b3730224ba79914b7ff780
Kledson Basso <kledsonhugo@gmail.com>
2023-10-18 18:12:55 -0300

    Terraform Multicloud

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -44,7 +44,7 @@ resource "aws_s3_object" "bucket-objects" {
   bucket       = aws_s3_bucket.bucket.id
   for_each     = fileset("../../app/", "*")
   key          = each.value
-  source       = "../app/${each.value}"
+  source       = "../../app/${each.value}"
   acl          = "public-read"
   content_type = "text/html"
   etag         = md5(file("../../app/${each.value}"))
