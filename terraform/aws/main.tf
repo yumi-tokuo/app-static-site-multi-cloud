ef6ead158ed87c632de10a645565dc152055a059
Kledson Basso <kledsonhugo@gmail.com>
2023-10-18 18:10:02 -0300

    Terraform Multicloud

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -2,25 +2,35 @@ resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
 }
 
-resource "aws_s3_bucket_versioning" "bucket-versioning" {
+resource "aws_s3_bucket_ownership_controls" "bucket-ownership" {
   bucket = aws_s3_bucket.bucket.id
-  versioning_configuration {
-    status = "Enabled"
+  rule {
+    object_ownership = "BucketOwnerPreferred"
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "bucket-public-access" {
+  bucket                  = aws_s3_bucket.bucket.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_acl" "bucket-acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.bucket-ownership,
+    aws_s3_bucket_public_access_block.bucket-public-access,
+  ]
   bucket = aws_s3_bucket.bucket.id
   acl    = "public-read"
 }
 
 resource "aws_s3_bucket_website_configuration" "bucket-website-configuration" {
   bucket = aws_s3_bucket.bucket.id
-
   index_document {
     suffix = "index.html"
   }
-
   error_document {
     key = "error.html"
   }
@@ -30,15 +40,19 @@ output "aws_s3_bucket_website_endpoint" {
   value = "http://${var.website_endpoint == "true" ? aws_s3_bucket_website_configuration.bucket-website-configuration.website_endpoint : ""}"
 }
 
-
-# RESOURCE: S3 BUCKET OBJECTS (APPLICATION)
-
 resource "aws_s3_object" "bucket-objects" {
   bucket       = aws_s3_bucket.bucket.id
   for_each     = fileset("../../app/", "*")
   key          = each.value
-  source       = "../../app/${each.value}"
+  source       = "../app/${each.value}"
   acl          = "public-read"
   content_type = "text/html"
   etag         = md5(file("../../app/${each.value}"))
+}
+
+resource "aws_s3_bucket_versioning" "bucket-versioning" {
+  bucket = aws_s3_bucket.bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
